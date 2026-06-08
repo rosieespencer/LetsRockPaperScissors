e819435d58033b24c6fc3b4d01d22d474b4539f2
rosieieio <182373827+rosieieio@users.noreply.github.com>
2026-06-08 13:40:01 -0400

    add function to check if any document elements are missing and function for countdown at start of game

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ const MOVES = new Map([
 ]);
 
 /**
+ * Check if at least one document element is missing.
+ */
+function checkDocElements() {
+    if (gameDisplay === null | winnerDisplay === null | playButton === button | rock === null | paper === null | scissors === null) {
+        throw new error ("One or more document element is missing.")
+    }
+}
+
+/**
  * Sets up starting screen where player can press the play button to begin the game.
  */
 function startScreen() {
@@ -25,11 +34,30 @@ function startScreen() {
 
     playButton.style.display = "block";
     playButton.textContent = "Play!";
-    playButton.addEventListener("click", startCountdonw, {once: true});
+    playButton.addEventListener("click", startCountdown, {once: true});
 
     rock.style.display = "none";
     paper.style.display = "none";
     scissors.style.display = "none";
 
     winnerDisplay.textContent = "";
+}
+
+/**
+ * Displays the words "Rock!", "Paper!", "Scissors!", "Shoot!", one at a time before starting the game.
+ */
+async function startCountdown() {
+    playButton.style.display = "none'";
+
+    let countdownIndex = 0;
+
+    while (countdownIndex < WORDS.length) {
+        const word = WORDS[countdownIndex];
+        if (word === undefined) {
+            throw new Error("Countdown word is undefined");
+        }
+        gameDisplay.textContent = word;
+        countdownIndex++;
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // display next word after 1 second
+    }
 }
